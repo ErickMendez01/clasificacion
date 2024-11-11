be8516f01813a36c80ac134f40b6911d1a251d06
ErickMendez01 <erickmendezgdl@gmail.com>
2024-11-10 21:45:46 -0600

    delete unnecessary folder

--- src/lib/schemas/loginSchema.ts
+++ src/lib/schemas/loginSchema.ts
@@ -2,7 +2,7 @@ import React from "react";
 import z, { TypeOf } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email({ message: "Correo invalido" }),
+  email: z.string().email({ message: "El correo electronico es invalido" }),
   password: z
     .string()
     .min(8, { message: "La contraseña debe tener al menos 8 caracteres" }),
